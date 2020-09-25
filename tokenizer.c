15889e834bd576cf7aceb9279fa7e1aaee746ec8
Whitney Alderman <wa125@ls.cs.rutgers.edu>
2020-09-25 19:23:53 -0400

    working on main

--- tokenizer.c
+++ tokenizer.c
@@ -46,8 +46,25 @@ int isHex(char *string){
   return 0;
 }
 
+//Function iterates through string to determine 
+//whether it is an int
+//Returns 0 for false or 1 for true
 int isInt(char *string){
-  return 0;
+int i = 0;
+
+while (string[i]!='\0'){
+
+//converts char to int
+int c = string[i]-'0';
+
+//reached a non int
+if (c<0 || c>9){
+return 0;
+}
+i++;
+}
+
+return 1;
 }
 
 int isFloat(char *string){
@@ -69,63 +86,77 @@ int size_input_string = strlen(argv[1]);
 //where we will copy input string into
 char *dest = malloc(sizeof(char)*((size_input_string)+1));
 
-//copy string from src and put it into destination
+//copy string from argv[1] and put it into destination
 strcpy(dest, argv[1]);
-
 printf("String Copied: %s\n", dest);
 
-
-char *currentstring = malloc(sizeof(char)*(size_input_string)+1);
-
-
 int i = 0;
-
+int j = 0;
 //iterate through input string char by char until the null terminator
-while(dest[i]!='\0'){
-//char c = argv[1][i];
-//printf("char: %c\n", c);
+while(argv[1][i]!='\0'){
 
-//each iteration, copies destination (argv[1]) into our current string, for a length of i
-strncpy(currentstring,dest,i+1);
+char *currentstring = malloc(sizeof(char)*(size_input_string)+1); 
 
+//each iteration, copies destination (argv[1]) into our current string, for a length of j+1
+strncpy(currentstring,dest,j+1);
 printf("Current Substring: %s\n", currentstring);
 	
-//substring(currentstring, dest, startofsubstring, lengthofsubstring);
+//initialize boolean values to false
+int word = 0;
+int octal = 0;
+int hex = 0;
+int integer = 0;
+int floatp = 0;
+int coperator = 0;
 
 if (isWord(currentstring)){
 puts("found word");
+word = 1;
 }
 if (isOctal(currentstring)){
 puts("found octal");
+octal = 1;
 }
 if(isHex(currentstring)){
 puts("found hex");
+hex = 1;
 }
 if(isInt(currentstring)){
 puts("found int");
+integer = 1;
 }
 if(isFloat(currentstring)){
 puts("found float");
+floatp = 1;
 }
 if(isC_Operator(currentstring)){
 puts("found C Operator");
+coperator = 1;
 }
 
 //Token does not qualify for any cases - the largest token is the previously found token
-if ((!isWord(currentstring)) && (!isOctal(currentstring)) && (!isHex(currentstring))
-&& (!isInt(currentstring)) && (!isFloat(currentstring)) && (!isC_Operator(currentstring))){
-
+if ((!word) && (!octal) && (!hex) && (!integer) && (!floatp) && (!coperator)){
+	
 
 //This copies the previouly found token and prints it	
-//commenting out for now since we need to write our methods first
+//will obviously return this for any methods not yet written
+// but it does work for test case: ./a.out 323t
+// and test case ./a.out 323t456
+char *token = malloc(sizeof(char)*(size_input_string)+1);  
+strncpy(token, currentstring, i);
+printf("Reached a non token, tokenizing previous token: %s\n", token);
 
-//strncpy(currentstring, dest, i);
-//printf("Reached a non token, tokenizing previous token: %s", currentstring);
-//must also start current string at appropriate index from here
+//resets substring pointer to first char after token
+int token_size = strlen(token);
+dest+=token_size;
+j=-1;
+
+//clears current string memory
+currentstring = "";
 }
 
 
-
+j++;
 i++;
 }
 // Token(dest[0]);	
