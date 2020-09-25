1f6faff6536404ad4dd7ccc1806cd24e8feaa84c
Gazal Arora <ga319@ls.cs.rutgers.edu>
2020-09-25 16:35:14 -0400

    token method

--- tokenizer.c
+++ tokenizer.c
@@ -3,6 +3,62 @@
 #include <ctype.h>
 #include <string.h>
 
+//Defining Node for Linked List
+struct Node{
+  char *token;
+  struct node *prev;
+  struct Node *next;
+};
+  struct Node* head;
+
+
+//Based on the first character of the string decides which Token method to send the string to
+//Returns a linked list of all tokens
+struct Node Token(char *string){
+  char c = string[0];
+    if((c>=65 && c<=90) || (c>=97 && c <= 122)){
+      //go to Word method
+    }
+
+    else if(c==48){
+      //go to Octal, Hex, Int and Float methods
+    }
+    else if(c>=49 && c<=57){
+      //go to Integer and Float method 
+    }
+    else{
+      //go to C operator method
+    }
+
+    return *head;
+}
+
+struct Node Word(char *string){
+  return *head;
+}
+
+struct Node Octal(char *string){
+  return *head;
+}
+
+struct Node Hex(char *string){
+  return *head;
+}
+
+struct Node Int(char *string){
+  return *head;
+}
+
+struct Node Float(char *string){
+  return *head;
+}
+
+struct Node C_Operator(char *string){
+  return *head;
+}
+
+
+
 int main(int argc, char** argv){
 
 //gets size of input string	
